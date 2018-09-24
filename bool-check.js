0b8a1caa026d56cb6d31365461e73e6224e9a303
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-09-23 22:22:18 -0400

    Add

---
+++ bool-check.js
@@ -0,0 +1,8 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    let b = typeof true;
+    return (typeof bool == b) ? true : false;
+}
+
+booWho(false);
+
